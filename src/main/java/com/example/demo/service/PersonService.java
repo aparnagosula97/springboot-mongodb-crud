f9c5c73f829b42fcfd09394426e228a6654ee8fd
Vijay, Kambala <vijay.kambala@optum.com>
2021-12-05 22:07:34 +0530

    autoid has also been added.

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -2,21 +2,37 @@ package com.example.demo.service;
 
 import com.example.demo.dao.PersonDao;
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.Optional;
 
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 @Service
 public class PersonService {
 
-    private final PersonDao personDao;
+    @Transient
+    public static final String SEQUENCE_NAME = "users_sequence";
 
-    public PersonService(PersonDao personDao) {
+    private final PersonDao personDao;
+    private final SequenceGeneratorService service;
+
+
+    public PersonService(@Autowired PersonDao personDao,
+                         @Autowired SequenceGeneratorService service) {
         this.personDao = personDao;
+        this.service = service;
     }
 
     public Person insertPersonData(Person person) {
+        person.setId(String.valueOf(service.getSequenceNumber(SEQUENCE_NAME)));
         return personDao.insertPersonData(person);
     }
 
@@ -35,5 +51,4 @@ public class PersonService {
     public void deletePersonUsingId(String id) {
         personDao.deletePersonUsingId(id);
     }
-
 }
