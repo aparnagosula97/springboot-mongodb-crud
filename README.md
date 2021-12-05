86eed5e21f06a99ea332842ce37bd4f379700fec
Vijay, Kambala <vijay.kambala@optum.com>
2021-12-05 22:07:14 +0530

    working copy. And autoid has also been added.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Spring Boot crud application using MongoDB
+1. Execute the below command in the mongo shell in order to create db. 
+Below command create database, and the spring boot application gets connected to 
+this db as it is used to the connection string.
+
+    cmd: use spring-mongo-crud
+    
--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -2,19 +2,27 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Document(collection = "Person")
 public class Person {
+
     @Id
     private String id;
     private String name;
+    private String email;
+    private String phone;
 
     public Person(@JsonProperty("id") String id,
-                  @JsonProperty("name") String name) {
+                  @JsonProperty("name") String name,
+                  @JsonProperty("email") String email,
+                  @JsonProperty("phone") String phone) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.phone = phone;
     }
 
     public String getId() {
@@ -33,4 +41,19 @@ public class Person {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
